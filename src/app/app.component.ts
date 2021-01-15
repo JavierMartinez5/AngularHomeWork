1540409859f38e750c137b820d5b12a1d4b5c1f9
Javier Martinez <rudiklindo1970@gmail.com>
2021-01-15 23:42:56 +0200

    Home work has done!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'homeWorkTimer';
+  time = 0;
+  counter = 0;
+  condition = false;
+  sub: Subscription;
+
+  onStartStopClick() {
+    this.condition = !this.condition;
+
+    if (this.condition) {
+      const stream$: Observable<number> = new Observable(obresrver => {
+        setInterval(() => {
+          obresrver.next(1);
+        }, 1000)
+      })
+      this.sub = stream$.subscribe(value => {
+        this.time += value;
+      })
+      this.counter = 0;
+    } else {
+      this.sub.unsubscribe();
+      this.time = 0;
+    }
+  }
+
+  onWaitClick() {
+    this.counter += 1;
+    console.log(this.counter);
+
+    if (this.counter === 1) { // For better optomization (prevent execute multiple SetTimeout)
+      setTimeout(() => {
+        if (this.counter > 1) {
+          this.sub.unsubscribe();
+          this.condition = false;
+        } else {
+          this.counter = 0;
+        }
+      }, 300);
+    }
+  }
+
+  onResetClick() {
+    this.time = 0;
+    
+    if (this.condition) return;
+
+    this.onStartStopClick();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TimerFormPipe } from './pipes/timer-form.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerFormPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
